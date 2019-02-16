d4d7703ec428393c2ab45e7f7e32aa35bc1ac532
gatomlo <thomas.verniers@myel.be>
2019-02-16 11:32:00 +0100

    add getReport schema in eventRepository

--- src/Gatomlo/ProjectManagerBundle/Repository/EventRepository.php
+++ src/Gatomlo/ProjectManagerBundle/Repository/EventRepository.php
@@ -18,19 +18,33 @@ class EventRepository extends \Doctrine\ORM\EntityRepository
     ->getResult();
   }
 
-  public function getReport($startDate,$endDate,$people,$project,$tag,$type)
+  public function getReport(array $filters = [])
   {
-    return $this->createQueryBuilder('e')
-    
-    ->where('e.project = :project')
-    ->setParameter(':project',$project)
-    ->andWhere('e.startDate BETWEEN :start AND :end')
-    ->setParameter('start', new \Datetime(date('Y').'-01-01'))  // Date entre le 1er janvier de cette année
-    ->setParameter('end',   new \Datetime(date('Y').'-12-31'))  // Et le 31 décembre de cette année
-
-
-    ->getQuery()
-    ->getResult();
-  }
+    $qb = $this->createQueryBuilder('e');
+    foreach ($filters as $filterName, $filterValue){
+       switch ($filterName) {
+          case 'project':
+            $qb->andWhere('e.project = :project');
+               ->setParameter(':project', $filterValue);
+            break;
+          case 'intervenant':
+            // join à faire avec people
+            break;
+          case 'tags':
+              //code à écrire
+              break;
+          case 'type':
+                //code à écrire
+                break;
+          case 'startDate':
+            $qb->andWhere('e.startDate BETWEEN :start AND :end');
+               ->setParameter('start', new \Datetime(date('Y').'-01-01')) // Date entre le 1er janvier de cette année
+               ->setParameter('end', new \Datetime(date('Y').'-12-31')) // Et le 31 décembre de cette année
+            break;
+          case 'endDate': //some code case 'people': //some code
+        }
+      }
+      return $qb->getQuery->getResult();
+    }
 
 }
